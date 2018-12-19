612b0838619931e8feaacc7073d639aa96d3baaa
kylepblum <20462558+kylepblum@users.noreply.github.com>
2018-12-18 18:52:14 -0600

    Updated driver to match bag & chain nomenclature

--- sarcSimDriver.m
+++ sarcSimDriver.m
@@ -1,10 +1,10 @@
-function [hsD,dataD,hsS,dataS] = sarcSimDriver(t,delta_f_activated,delta_cdl)
+function [hsB,dataB,hsC,dataC] = sarcSimDriver(t,delta_f_activated,delta_cdl)
 time_step = t(2)-t(1);
 CHECK_SLACK = 1;  
 
 % Make a half-sarcomere
-hsD = halfSarcBag();
-hsS = halfSarcChain();
+hsB = halfSarcBag();
+hsC = halfSarcChain();
 
 % Loop through the time-steps
 for a = 1
@@ -17,25 +17,25 @@ for a = 1
             %CHECK IF SLACK
             %ADJUST HS LENGTH SO FORCE IS BACK TO 0
 %             hs.hs_force = 0;
-            xD = find_hsl_from_force(hsD,0.0);
-            xS = find_hsl_from_force(hsS,0.0);
+            xD = find_hsl_from_force(hsB,0.0);
+            xS = find_hsl_from_force(hsC,0.0);
           
             
-            if hsD.cmd_length<xD
-                hsD.hs_force = 0;
+            if hsB.cmd_length<xD
+                hsB.hs_force = 0;
                 slack_modeD = 1;
 %                 new_length = max(x,hs.cmd_length);
-                adj_lengthD = xD - hsD.hs_length;
-                hsD.forwardStep(0.0,adj_lengthD,0,0,0,1);
+                adj_lengthD = xD - hsB.hs_length;
+                hsB.forwardStep(0.0,adj_lengthD,0,0,0,1);
             else
                 slack_modeD = 0;
             end
             
-            if hsS.cmd_length<xS
-                hsS.hs_force = 0;
+            if hsC.cmd_length<xS
+                hsC.hs_force = 0;
                 slack_modeS = 1;
-                adj_lengthS = xS - hsS.hs_length;
-                hsS.forwardStep(0.0,adj_lengthS,0,0,0,1);
+                adj_lengthS = xS - hsC.hs_length;
+                hsC.forwardStep(0.0,adj_lengthS,0,0,0,1);
             else
                 slack_modeS = 0;
             end
@@ -46,11 +46,11 @@ for a = 1
                % against zero load.
                 
                % First, we evolve the distribution
-                hsD.forwardStep(time_step,0.0,0.0,delta_f_activated(a,i),1,0)
+                hsB.forwardStep(time_step,0.0,0.0,delta_f_activated(a,i),1,0)
                 
                % Next, we iteratively search for the sarcomere length
                % that would give us zero load
-                xD = find_hsl_from_force(hsD,0);
+                xD = find_hsl_from_force(hsB,0);
                 
                % We then compute the new hs length applied to the
                % sarcomere based on whether the command length is now
@@ -59,15 +59,15 @@ for a = 1
                % length adjustment is the the calculated new length - the 
                % current measurement of hs length. 
                
-                new_lengthD = max(xD,hsD.cmd_length);
-                adj_lengthD = new_lengthD - hsD.hs_length;
+                new_lengthD = max(xD,hsB.cmd_length);
+                adj_lengthD = new_lengthD - hsB.hs_length;
                 
                % Finally, we shift the distribution by the adjusted length
-                hsD.forwardStep(time_step,adj_lengthD,delta_cdl(a,i),0,0,1);
+                hsB.forwardStep(time_step,adj_lengthD,delta_cdl(a,i),0,0,1);
                 
             else %length control
                 delta_hslD = delta_cdl(a,i);
-                hsD.forwardStep(time_step,delta_hslD,delta_cdl(a,i),delta_f_activated(a,i),1,1);
+                hsB.forwardStep(time_step,delta_hslD,delta_cdl(a,i),delta_f_activated(a,i),1,1);
             end
             
             if slack_modeS
@@ -76,11 +76,11 @@ for a = 1
                 % against zero load.
                 
                 % First, we evolve the distribution
-                hsS.forwardStep(time_step,0.0,0.0,delta_f_activated(a,i),1,0)
+                hsC.forwardStep(time_step,0.0,0.0,delta_f_activated(a,i),1,0)
                 
                 % Next, we iteratively search for the sarcomere length
                 % that would give us zero load
-                xS = find_hsl_from_force(hsS,0);
+                xS = find_hsl_from_force(hsC,0);
                 
                 % We then compute the new hs length applied to the
                 % sarcomere based on whether the command length is now
@@ -89,52 +89,52 @@ for a = 1
                 % length adjustment is the the calculated new length - the
                 % current measurement of hs length.
                 
-                new_lengthS = max(xS,hsS.cmd_length);
-                adj_lengthS = new_lengthS - hsS.hs_length;
+                new_lengthS = max(xS,hsC.cmd_length);
+                adj_lengthS = new_lengthS - hsC.hs_length;
                 
                 % Finally, we shift the distribution by the adjusted length
-                hsS.forwardStep(time_step,adj_lengthS,delta_cdl(a,i),0,0,1);
+                hsC.forwardStep(time_step,adj_lengthS,delta_cdl(a,i),0,0,1);
                 
             else %length control
                 delta_hslS = delta_cdl(a,i);
-                hsS.forwardStep(time_step,delta_hslS,delta_cdl(a,i),delta_f_activated(a,i),1,1);
+                hsC.forwardStep(time_step,delta_hslS,delta_cdl(a,i),delta_f_activated(a,i),1,1);
             end
         end
         
         % Store data
         
-        dataD(a).f_activated(i) = hsD.f_activated;
-        dataD(a).f_bound(i) = hsD.f_bound;
-        dataD(a).f_overlap(i) = hsD.f_overlap;
-        dataD(a).cb_force(i) = hsD.cb_force;
+        dataB(a).f_activated(i) = hsB.f_activated;
+        dataB(a).f_bound(i) = hsB.f_bound;
+        dataB(a).f_overlap(i) = hsB.f_overlap;
+        dataB(a).cb_force(i) = hsB.cb_force;
         
         
-        dataD(a).passive_force(i) = hsD.passive_force;
-        dataD(a).hs_force(i) = hsD.hs_force;
-        dataD(a).hs_length(i) = hsD.hs_length;
-        dataD(a).cmd_length(i) = hsD.cmd_length;
-        dataD(a).Ca(i) = hsD.Ca;
-        dataD(a).slack(i) = hsD.slack;
-        dataD(a).bin_pops(:,i) = hsD.bin_pops;
-        dataD(a).no_detached(i) = hsD.no_detached;
+        dataB(a).passive_force(i) = hsB.passive_force;
+        dataB(a).hs_force(i) = hsB.hs_force;
+        dataB(a).hs_length(i) = hsB.hs_length;
+        dataB(a).cmd_length(i) = hsB.cmd_length;
+        dataB(a).Ca(i) = hsB.Ca;
+        dataB(a).slack(i) = hsB.slack;
+        dataB(a).bin_pops(:,i) = hsB.bin_pops;
+        dataB(a).no_detached(i) = hsB.no_detached;
         
-        dataS(a).f_activated(i) = hsS.f_activated;
-        dataS(a).f_bound(i) = hsS.f_bound;
-        dataS(a).f_overlap(i) = hsS.f_overlap;
-        dataS(a).cb_force(i) = hsS.cb_force;
+        dataC(a).f_activated(i) = hsC.f_activated;
+        dataC(a).f_bound(i) = hsC.f_bound;
+        dataC(a).f_overlap(i) = hsC.f_overlap;
+        dataC(a).cb_force(i) = hsC.cb_force;
         
         
-        dataS(a).passive_force(i) = hsS.passive_force;
-        dataS(a).hs_force(i) = hsS.hs_force;
-        dataS(a).hs_length(i) = hsS.hs_length;
-        dataS(a).cmd_length(i) = hsS.cmd_length;
-        dataS(a).Ca(i) = hsS.Ca;
-        dataS(a).slack(i) = hsS.slack;
-        dataS(a).bin_pops(:,i) = hsS.bin_pops;
-        dataS(a).no_detached(i) = hsS.no_detached;
+        dataC(a).passive_force(i) = hsC.passive_force;
+        dataC(a).hs_force(i) = hsC.hs_force;
+        dataC(a).hs_length(i) = hsC.hs_length;
+        dataC(a).cmd_length(i) = hsC.cmd_length;
+        dataC(a).Ca(i) = hsC.Ca;
+        dataC(a).slack(i) = hsC.slack;
+        dataC(a).bin_pops(:,i) = hsC.bin_pops;
+        dataC(a).no_detached(i) = hsC.no_detached;
         
     end
-    dataD(a).t = t;
-    dataS(a).t = t;
+    dataB(a).t = t;
+    dataC(a).t = t;
 end
 
