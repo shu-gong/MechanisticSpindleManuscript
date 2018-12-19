85da6e5cf859b4257d9275bb1e5f9592d343a45d
kylepblum <20462558+kylepblum@users.noreply.github.com>
2018-12-18 18:54:47 -0600

    Updated sarc2spindle to include bag/chain nomenclature

--- sarc2spindle.m
+++ sarc2spindle.m
@@ -1,14 +1,14 @@
-function [r,rs,rd] = sarc2spindle(dataD,dataS,kSs,kSd,kD,competition,threshold)
-t = dataS.t; %time
+function [r,rs,rd] = sarc2spindle(dataB,dataC,kFc,kFb,kYb,occlusion,threshold)
+t = dataC.t; %time
 
 % Force-dominant fiber (static fiber)
-Fs = dataS.hs_force;
+Fs = dataC.hs_force;
 Fs(Fs<0) = 0;
 
 
 
 % Yank-dominant fiber (dynamic fiber)
-Fd = dataD.hs_force; %force
+Fd = dataB.hs_force; %force
 Fd(Fd<0) = 0; %threshold
 Y = diff(Fd)./diff(t); %yank
 Y(Y<0) = 0; %threshold
@@ -17,14 +17,14 @@ Y(Fd<8e4) = 0; %Chirp
 % Y(Fd<8e4) = 0; %Most of the simulations
 
 
-rs = Fs*kSs; %static component
-rd = Fd*kSd + Y*kD; %dynamic component
+rs = Fs*kFc; %static component
+rd = Fd*kFb + Y*kYb; %dynamic component
 
 rs(rs<0) = 0;
 rs = rs/(10^6);
 rd = rd/(10^6);
 
-if competition % Hypothesis that branches of Ia ending compete for total firing rate
+if occlusion % Hypothesis that branches of Ia ending compete for total firing rate
     rsComp = rs;
     rsComp(rd>=rs) = 0.3*rsComp(rd>=rs);  % 0.3 chosen to match Banks et al. 1997
     
